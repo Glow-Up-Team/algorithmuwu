e24fca4ee0dea2425e920e94e1f2ee3dec3581b5
hass0me <cescor.0503@gmail.com>
2022-04-01 09:09:22 -0600

    added uwu

--- min.c
+++ min.c
@@ -1,1 +1,13 @@
-//uwuntus
+int min(int arr[], int len) {
+    int indexMin = 0;
+    for (int i = 0; i< len; i++){
+
+        if (arr[i] > arr[indexMin]){
+            indexMin = i;
+        };
+
+    };
+    
+    return indexMin;
+};
+//uwu
