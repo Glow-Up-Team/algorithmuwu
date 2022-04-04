0137380830747367f00e36e071dc15ad719d305e
hass0me <cescor.0503@gmail.com>
2022-04-04 11:19:52 -0500

    Just min.h

--- min2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "min.h"
-
-int main(){
-	int numeros[5] = {596, 758, 465, 864, 687};
-
-	printf("El menor es %d\n", numeros[min(numeros, 5)]  );
-}
--- min3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "min.h"
-
-int arr[] = {1, 2, 3};
-
-int main(){
-    printf("%d\n", min(arr, 1));
-}
