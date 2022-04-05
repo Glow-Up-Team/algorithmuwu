345fc2b790f9422e023af203d0287efe88be80eb
elmau21 <elmau0213@gmail.com>
2022-04-05 09:52:22 -0500

    Modified main.c and added .h files

--- main.c
+++ main.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
+#include "odd3.h"
+#include "find.h"
+#include "min.h"
+#include "max.h"
 
+
+int arr[] = {315,153,135,12};
+int len = sizeof(arr)/sizeof(arr[0]);
+int catch = 153;
+int myArray[3];
+
+int main(){
+    int index = find(arr, len, catch);
+    printf("The number %d was found in the index %d \n", arr[index], index);
+
+    int numbersOdd = odd(myArray, len);
+    printf("%d\n", numbersOdd);
+
+    int maxIndex = max(arr, len);
+	printf("Max number Value: %d\n", arr[maxIndex], maxIndex);
+
+    printf("El menor es %d\n", numeros[min(numeros, 5)]);
+}
